ec7be41d3ef7c4dbf084960db46b945703a510ee
Austin Paxton <austin.is.paxton@gmail.com>
2015-07-09 12:28:41 -0500

    whee full keybindings. Still need to do r1 and l1 as modifiers

---
+++ controller_debug.rb
@@ -0,0 +1,54 @@
+require 'artoo'
+
+connection :joystick, :adaptor => :joystick
+device :joystick, :driver => :ps3, :connection => :joystick, :interval => 0.1
+
+work do
+  on joystick, :button_triangle => proc { |*value|
+    puts 'triangle'
+  }
+  on joystick, :button_square => proc { |*value|
+    puts 'square'
+  }
+  on joystick, :button_circle => proc { |*value|
+    puts 'circle'
+  }
+  on joystick, :button_x => proc { |*value|
+    puts 'ex'
+  }
+  on joystick, :joystick => proc { |caller, value|
+    if value[:s] == 0 then
+      if value[:x] > 25000 then
+        puts 'left stick right'
+      elsif value[:x] < -25000 then
+        puts 'left stick left'
+      elsif value[:y] > 25000 then
+        puts 'left stick down'
+      elsif value[:y] < -25000 then
+        puts 'left stick up'
+      end
+    elsif value[:s] == 1 then
+      if value[:x] > 25000 then
+        puts 'right stick right'
+      elsif value[:x] < -25000 then
+        puts 'right stick left'
+      elsif value[:y] > 25000 then
+        puts 'right stick down'
+      elsif value[:y] < -25000 then
+        puts 'right stick up'
+      end
+    end
+  }
+  on joystick, :button => proc { |*value|
+    if value[1] == 4 then
+      puts 'dpad up'
+    elsif value[1] == 6 then
+      puts 'dpad down'
+    elsif value[1] == 7 then
+      puts 'dpad left'
+    elsif value[1] == 5 then
+      puts 'dpad right'
+    end
+  }
+  on joystick, :button
+end
--- controller_test.rb
+++ controller_test.rb
@@ -19,27 +19,67 @@ device :joystick, :driver => :ps3, :connection => :joystick, :interval => 0.1
 
 work do
   on joystick, :button_triangle => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'forward')
+    res = http.post_form(uri, 'sessionId' => id, 'command' => 'up')
+    puts 'triangle'
   }
   on joystick, :button_square => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'left')
+    res = http.post_form(uri, 'sessionId' => id, 'command' => 'suck')
+    puts 'square'
   }
   on joystick, :button_circle => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'right')
+    res = http.post_form(uri, 'sessionId' => id, 'command' => 'drop')
+    puts 'circle'
   }
   on joystick, :button_x => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'back')
+    res = http.post_form(uri, 'sessionId' => id, 'command' => 'down')
+    puts 'ex'
   }
-  on joystick, :button_up => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'test')
+  on joystick, :joystick => proc { |caller, value|
+    if value[:s] == 0 then
+      if value[:x] > 25000 then
+        res = http.post_form(uri, 'sessionId' => id, 'command' => 'place')
+        puts 'left stick right'
+      elsif value[:x] < -25000 then
+        puts 'left stick left'
+      elsif value[:y] > 25000 then
+        res = http.post_form(uri, 'sessionId' => id, 'command' => 'placeDown')
+        puts 'left stick down'
+      elsif value[:y] < -25000 then
+        res = http.post_form(uri, 'sessionId' => id, 'command' => 'placeUp')
+        puts 'left stick up'
+      end
+    elsif value[:s] == 1 then
+      if value[:x] > 25000 then
+        res = http.post_form(uri, 'sessionId' => id, 'command' => 'dig')
+        puts 'right stick right'
+      elsif value[:x] < -25000 then
+        puts 'right stick left'
+      elsif value[:y] > 25000 then
+        res = http.post_form(uri, 'sessionId' => id, 'command' => 'digDown')
+        puts 'right stick down'
+      elsif value[:y] < -25000 then
+        res = http.post_form(uri, 'sessionId' => id, 'command' => 'digUp')
+        puts 'right stick up'
+      end
+    end
   }
-  on joystick, :button_down => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'test')
+  on joystick, :button => proc { |*value|
+    if value[1] == 4 then
+      res = http.post_form(uri, 'sessionId' => id, 'command' => 'forward')
+      puts 'dpad up'
+    elsif value[1] == 6 then
+      res = http.post_form(uri, 'sessionId' => id, 'command' => 'back')
+      puts 'dpad down'
+    elsif value[1] == 7 then
+      res = http.post_form(uri, 'sessionId' => id, 'command' => 'left')
+      puts 'dpad left'
+    elsif value[1] == 5 then
+      res = http.post_form(uri, 'sessionId' => id, 'command' => 'right')
+      puts 'dpad right'
+    end
   }
-  on joystick, :button_left => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'test')
-  }
-  on joystick, :button_right => proc { |*value|
-    res = http.post_form(uri, 'sessionId' => id, 'command' => 'test')
+  on joystick, :button_r1 => proc { |*value|
+    res = http.post_form(uri, 'sessionId' => id, 'command' => 'rs')
+    puts 'button r1'
   }
 end
